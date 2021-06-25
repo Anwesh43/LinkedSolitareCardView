6d851e1c0ecab7a0b5ffa6a31b4805509ef417de
Anwesh43 <anweshthecool0@gmail.com>
2021-06-26 01:38:58 +0530

    adding constants and libraies

--- solitarecardview/src/main/java/com/example/solitarecardview/SolitareCardView.kt
+++ solitarecardview/src/main/java/com/example/solitarecardview/SolitareCardView.kt
@@ -1,2 +1,28 @@
 package com.example.solitarecardview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#004D40",
+    "#FFD600",
+    "#00C853",
+    "#6200EA"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
+val strokeFactor : Float = 90f
+val cardWFactor : Float = 8.9f
+val cardHFactor : Float = 3.9f
+val delay : Long = 20
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+
