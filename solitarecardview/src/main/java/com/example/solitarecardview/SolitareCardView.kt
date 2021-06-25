5a15db834bb8fb6742802f23f24da0c133b95b73
Anwesh43 <anweshthecool0@gmail.com>
2021-06-26 02:01:43 +0530

    adding Canvas extension methods

--- solitarecardview/src/main/java/com/example/solitarecardview/SolitareCardView.kt
+++ solitarecardview/src/main/java/com/example/solitarecardview/SolitareCardView.kt
@@ -25,9 +25,50 @@ val cardHFactor : Float = 3.9f
 val delay : Long = 20
 val parts : Int = 3
 val scGap : Float = 0.02f / parts
+val cards : Int = 3
 
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, (this - i * n.inverse()))
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawSolitareCard(scale : Float, w : Float, h : Float, paint : Paint) {
+    val cardW : Float = Math.min(w, h) / cardWFactor
+    val cardH : Float = Math.min(w, h) / cardHFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val gap : Float = cardH / cards
+    save()
+    for (i in 0..(cards - 1)) {
+        save()
+        translate(0f, -gap * (6 - i) * sc2 - (h / 2 + cardH) * sc3)
+        drawLine(
+            -cardW / 2,
+            -cardH / 2,
+            -cardW / 2 + cardW * sc1,
+            -cardH / 2, paint
+        )
+        drawRect(
+            RectF(
+                -cardW / 2,
+                -cardH / 2,
+                cardW / 2,
+                -cardH / 2 + cardH * sc1
+            ),
+            paint
+        )
+        restore()
+    }
+    restore()
+
+}
+
+fun Canvas.drawSCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawSolitareCard(scale, w, h, paint)
+}
